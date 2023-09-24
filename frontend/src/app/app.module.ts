ac251b1edab608927d872a6a3e03f5768171d994
B6304416 <natthapoln27@gmail.com>
2023-09-24 15:46:55 +0700

    login

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
+
+
+import { AppRoutingModule } from './app-routing.module'; // นำเข้า AppRoutingModule
+import { RouterModule } from '@angular/router'; // นำเข้า RouterModule
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
+    LoginComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
