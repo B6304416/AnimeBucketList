d68b73e2b0ccfdf02b0d3012fda20642d57abda5
tunt7 <thanadetpfg@gmail.com>
2023-09-30 06:12:17 +0700

    add show popular character

--- frontend/src/app/components/animelist/animelist.component.ts
+++ frontend/src/app/components/animelist/animelist.component.ts
@@ -5,8 +5,14 @@ import { Router } from '@angular/router';
 interface AnimeResponse {
   name: string;
   episode: number;
-  genre:string;
-  imgUrl:string;
+  genre: string;
+  imgUrl: string;
+}
+
+interface PopCharacterResponse {
+  name: string;
+  score: number;
+  imgUrl: string;
 }
 
 @Component({
@@ -19,6 +25,7 @@ export class AnimelistComponent implements OnInit {
 
   // data!: string;
   data: AnimeResponse[] = [];
+  popCharacter: PopCharacterResponse[] = [];
 
   constructor(private http: HttpClient, private router: Router) {
   }
@@ -31,6 +38,19 @@ export class AnimelistComponent implements OnInit {
         this.data = res
         console.log(this.data)
         this.addNewPost();
+      },
+      (error) => {
+        console.error('Error:', error);
+      }
+    );
+
+    const char_url = 'http://localhost:5555/character/popular';
+    this.http.get<PopCharacterResponse[]>(char_url).subscribe(
+      (res) => {
+        console.log('Response data:', res);
+        this.popCharacter = res
+        console.log(this.popCharacter)
+        this.addPopCharacter();
       },
       (error) => {
         console.error('Error:', error);
@@ -67,4 +87,35 @@ export class AnimelistComponent implements OnInit {
       })
     }
   }
+
+  // Function to create PopCharacter
+  createPopCharacter(data: PopCharacterResponse): HTMLElement {
+    const card = document.createElement("div");
+    // card.className = "col-lg-4";
+    card.innerHTML = `
+          <div class="card mb-4">
+            <a href="#!"><img class="card-img-top"
+                    src="${data.imgUrl}"
+                    alt="..." /></a>
+            <div class="card-body">
+                <div class="small text-muted">POPULAR CHARACTER</div>
+                <h2 class="card-title">${data.name}</h2>
+                <p class="card-text">SCORE: ${data.score}</p>
+                <a class="btn btn-primary" href="#!">Read more →</a>
+            </div>
+        </div>
+    `;
+    return card;
+  }
+
+  addPopCharacter(): void {
+    const PopCharacterResponse = document.getElementById("card-pop-character");
+    if (PopCharacterResponse) {
+      this.popCharacter.forEach((item) => {
+        const newCard = this.createPopCharacter(item);
+        PopCharacterResponse.appendChild(newCard);
+      })
+    }
+  }
+
 }
