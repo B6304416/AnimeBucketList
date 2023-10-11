27c177d22f48c03787856efbeddae1f23deccbc0
tunt7 <thanadetpfg@gmail.com>
2023-10-11 15:04:36 +0700

    recheck

--- backend/index.js
+++ backend/index.js
@@ -32,8 +32,8 @@ app.use(
     cors({
         origin: 'http://localhost:3000',
         methods: ['GET', 'POST', 'PUT', 'DELETE'],
-        allowedHeaders: ['Content-Type', 'Authorization', 'UserRole'],
-        credentials: true,
+        allowedHeaders: ['Content-Type', 'Authorization'],
+        // credentials: true,
     })
 )
 
