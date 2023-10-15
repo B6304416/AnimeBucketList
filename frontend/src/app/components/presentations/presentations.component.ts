b296f7e8e03eef734ab0785247d8b99016d1ffbc
B6304416 <natthapoln27@gmail.com>
2023-10-16 00:00:42 +0700

    present

--- frontend/src/app/components/presentations/presentations.component.ts
+++ frontend/src/app/components/presentations/presentations.component.ts
@@ -6,15 +6,20 @@ import { SharedDataService } from 'src/app/services/shared-data.service';
   templateUrl: './presentations.component.html',
   styleUrls: ['./presentations.component.css']
 })
-export class PresentationsComponent implements OnInit{
+export class PresentationsComponent implements OnInit {
 
   constructor(
     private sharedDataService: SharedDataService
-  ) { }
+  ) {}
 
   ngOnInit(): void {
-      this.sharedDataService.setIsLoginPage(true);
+    this.sharedDataService.setIsLoginPage(true);
   }
-
-  
+  openFullscreen() {
+    const elem = document.documentElement;
+    if (elem.requestFullscreen) {
+      elem.requestFullscreen();
+      console.log("full")
+    }
+  }
 }
